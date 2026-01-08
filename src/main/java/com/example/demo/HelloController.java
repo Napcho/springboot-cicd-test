6911c5d7a6437d9f76af2db9796fa937c95d1814
Napcho <rmskr33@gmail.com>
2026-01-08 13:51:58 +0530

    Add comment for new changes in HelloController

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//new changes
 @RestController
 public class HelloController {
     @GetMapping("/hello")
